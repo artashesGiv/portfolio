afbcb64c4d585c37ee87677dec79cd2c463b8391
nayaa <artashes.giv@gmail.com>
2021-12-21 13:03:11 +0300

    gh-pages1

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,7 +9,6 @@ export const Footer = () => {
             <a href="https://instagram.com">Instagram</a>
             <a href="https://telegram.com">Telegram</a>
             <a href="https://linkedin.com">LinkedIN</a>
-
          </div>
       </div>
    )
