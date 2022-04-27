e28e5859a8db9e7ace67569047668783e8284d03
nayaa <artashes.giv@gmail.com>
2022-04-27 18:48:13 +0300

    scroll to anchor link

--- src/components/Menu/Nav/Nav.tsx
+++ src/components/Menu/Nav/Nav.tsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import s from '../Menu.module.scss'
 
-export const Nav = ({className = s.nav}:{className?: string}) => {
+export const Nav = ({className = s.nav}: { className?: string }) => {
+
+   useEffect(() => {
+
+      const scrollTo = (element: any) => {
+         window.scroll({
+            left: 0,
+            top: element.offsetTop,
+            behavior: 'smooth',
+         })
+      }
+
+      document.querySelectorAll('.anchor').forEach(link => {
+         link.addEventListener('click', e => {
+            e.preventDefault()
+
+            const href = link.getAttribute('href')?.substring(1)
+            const scrollTarget = document.getElementById(href || '')
+            scrollTo(scrollTarget)
+         })
+      })
+   })
+
    return (
       <div className={className}>
          <ul>
-            <li><a href={'#hello'}>Hello</a></li>
-            <li><a href={'#resume'}>Skills</a></li>
-            <li><a href={'#portfolio'}>Portfolio</a></li>
-            <li><a href={'#contact'}>Contact</a></li>
+            <li><a className={'anchor'} href={'#hello'}>Hello</a></li>
+            <li><a className={'anchor'} href={'#resume'}>Skills</a></li>
+            <li><a className={'anchor'} href={'#portfolio'}>Portfolio</a></li>
+            <li><a className={'anchor'} href={'#contact'}>Contact</a></li>
          </ul>
       </div>
    )
